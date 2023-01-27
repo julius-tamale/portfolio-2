feb944714285f24d7efc3af2add4be86f1769494
julius-tamale <juliotamex@proton.me>
2023-01-27 09:07:46 +0200

    portifolio 2

---
+++ assets/scripts/script.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll('section');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document
+            .querySelector('.nav__menu a[href*=' + sectionId + ']')
+            .classList.add('active__link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+            .classList.remove('active__link');
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
